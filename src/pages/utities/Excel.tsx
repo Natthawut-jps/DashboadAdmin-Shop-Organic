7187fbb572b0e6bd9bef745b19e02cf6842dbdae
Natthawut Jampasaeng <121080181+Natthawut-jps@users.noreply.github.com>
2024-05-02 16:13:45 +0700

    up

--- src/pages/utities/Excel.tsx
+++ src/pages/utities/Excel.tsx
@@ -1,35 +1,35 @@
 import { saveAs } from "file-saver";
 import instance_auth from "./instance_auth";
 
-// const Excel = () => {
-//   (async () => {
-//     await instance_auth({
-//       method: "get",
-//       url: "/excel/order",
-//       responseType: "blob"
-//     }).then((res) => {
-//       if (res.status === 200) {
-//         console.log(res);
-//         saveAs(res.data, "example.xlsx");
-//       }
-//     });
-//   })();
-// };
-
 const Excel = () => {
   (async () => {
     await instance_auth({
       method: "get",
-      url: "/pdf/order",
+      url: "/excel/order",
       responseType: "blob"
     }).then((res) => {
       if (res.status === 200) {
         console.log(res);
-        saveAs(res.data, "example.pdf");
+        saveAs(res.data, "example.xlsx");
       }
     });
   })();
 };
+
+// const Excel = () => {
+//   (async () => {
+//     await instance_auth({
+//       method: "get",
+//       url: "/pdf/order",
+//       responseType: "blob"
+//     }).then((res) => {
+//       if (res.status === 200) {
+//         console.log(res);
+//         saveAs(res.data, "example.pdf");
+//       }
+//     });
+//   })();
+// };
 
 
 export default Excel;
